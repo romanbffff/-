805660bdd0626897589bd69d87120eec1953857b
Sasha Chmelyuk <102144771+SashaChmelyuk@users.noreply.github.com>
2022-05-27 13:06:04 +0300

    Bux fix

--- php/Authorization/contact.php
+++ php/Authorization/contact.php
@@ -23,7 +23,33 @@
              <a class="lang" key="city-vn"> Вінниця, Вінницька область,21000</a><br>
               </p>
             </div> 
-           </section> 
+           </section>
+           <h1 class="lang h_cont" key="">Форма зворотнього зв'язку</h1> 
+           <section class="form">
+           <form action="telegram.php" method="POST">
+
+                <div class="form-group">
+                    <label for="">Введіть ваше ім'я:</label>
+                    <input type="text" class="form-control" id="" name="user_name" placeholder="Name">
+                </div>
+            
+                <div class="form-group">
+                    <label for="">Введіть ваш номер телефона:</label>
+                    <input type="tel" class="form-control" id="" name="user_phone" placeholder="+38 (099) 99 99 999" pattern="^+380\d{3}\d{2}\d{2}\d{2}$">
+                </div>
+            
+                <div class="form-group">
+                    <label for="">Введіть email:</label>
+                    <input type="email" class="form-control" id="" name="user_email" placeholder="mail@mail.com">
+                </div>
+            <div class="form-group">
+              <label for="" class="mes">Введіть повідомлення:</label>
+              <textarea placeholder="Message" rows="3" name="user_message"></textarea>
+            </div>
+               
+                <button type="submit" class="btn btn-primary">Відправити</button>
+            </form>
+        </section>
      </body>
     <footer class="f-contact"><p>JetIKy &copy; 2022</p></footer>
 </html>
---
+++ php/Authorization/telegram.php
@@ -0,0 +1,30 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$message = $_POST['user_message'];
+$token = "5237546248:AAGLWukTGCcciTOCr6OCE88rgSA74tVD72I";
+$chat_id = "-1001686992917";
+$arr = array(
+  "Ім'я користувача: " => $name,
+  'Телефон: ' =>$phone,
+  'Email: ' => $email,
+  'Повідомлення: '=> $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: contact.php');
+} else {
+  echo "Error";
+}
+?>
